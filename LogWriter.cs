d3894ebdf75597411a4bd83e22a83999d6951ace
archieangeles@gmail.com <archieangeles@gmail.com>
2022-03-16 00:46:23 +0800

    updated to logger

---
+++ LogWriter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pachie.App.Logger
+{
+    public class LogWriter
+    {
+        string _logPath;
+        public LogWriter(string logPath)
+        {
+            _logPath = logPath;
+        }
+
+
+        public void Write(string message)
+        {
+            try
+            {
+                using (System.IO.StreamWriter sw = new System.IO.StreamWriter(_logPath, true))
+                {
+                    sw.WriteLine("{0} --> {1}", DateTime.Now, message);
+                }
+
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+    }
+}
